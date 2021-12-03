a7751a6f1625e67a3468c9756d0d5796ee930660
ZeroDiscord <87260173+ItsZeroXMC@users.noreply.github.com>
2021-12-03 19:04:33 +0800

    Add files via upload

---
+++ commands/invite.js
@@ -0,0 +1,19 @@
+const Discord = require("discord.js");
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+  name: "invite",
+  aliases: ["add", "inv"],
+  description: "",
+  usage: "",
+  run: async (client, message, args) => {
+    //Start
+    const embed = new MessageEmbed()
+      .setColor("#3d8eff")
+      .setDescription(`[Add Reaction Role in your server](https://discord.com/oauth2/authorize?client_id=${client.userid}&scope=bot&permissions=8)`)
+
+    message.channel.send(embed);
+
+    //End
+  }
+};
