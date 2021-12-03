d06982becdcdcbe814ab4e85b02994eac6441c32
ZeroDiscord <87260173+ItsZeroXMC@users.noreply.github.com>
2021-12-03 19:14:08 +0800

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -10,7 +10,7 @@ module.exports = {
     //Start
     const embed = new MessageEmbed()
       .setColor("RANDOM")
-      .setDescription(`**Reaction Commands**\n\`👥\` \`rr!add <#channeL> <MESSAGEID> <@ROLE> <EMOJI>\`\n*Make a reaction that gives out a role when you react!*\n\`👤\` \`rr!delete <MESSAGEID> <EMOJI>\`\n*Remove your reaction roles*\n**Information Commands**\n\`🏓\` \`rr!ping\`\n*Gives you information on how fast the Bot can respond to you*\n\`📝\` \`rr!help\`\n*A list of my commands!*\n\`✉️\` \`invite\` - *Invite Reaction role invite server*`)
+      .setDescription(`**Reaction Commands**\n\`👥\` \`rr!add <#channeL> <MESSAGEID> <@ROLE> <EMOJI>\`\n*Make a reaction that gives out a role when you react!*\n\`👤\` \`rr!delete <MESSAGEID> <EMOJI>\`\n*Remove your reaction roles*\n**Information Commands**\n\`🏓\` \`rr!ping\`\n*Gives you information on how fast the Bot can respond to you*\n\`📝\` \`rr!help\`\n*A list of my commands!*\n\`✉️\` \`invite\`\n*Invite Reaction role invite server*`)
 
     message.channel.send(embed);
 
